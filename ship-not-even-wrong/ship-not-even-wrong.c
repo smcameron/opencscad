bb3f011f97414f13678496bc8d1104cae7c96df7
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-08-10 09:41:12 -0500

    Add some randomness functions

--- ship-not-even-wrong/ship-not-even-wrong.c
+++ ship-not-even-wrong/ship-not-even-wrong.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "opencscad.h"
+
+static int randn(int n)
+{
+	return (int) ((float) n * (float) rand() / (float) RAND_MAX);
+}
 
 static void cylindrical_thing(float length, float r1, float r2)
 {
@@ -18,8 +24,12 @@ static void cylindrical_module(char *modulename, float length, float r1, float r
 	
 int main(int argc, char *argv[])
 {
+	int i;
+
 	cylindrical_module("testthing", 10, 5, 3);
 	call_module("testthing");
+	for (i = 0; i < 100; i++)
+		printf("%d\n", randn(20));
 	return 0;
 }
 

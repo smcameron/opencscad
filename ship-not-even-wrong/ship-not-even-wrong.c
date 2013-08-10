1413eaf43bbb082de741c3518a588b27b912ab5a
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-08-10 10:10:57 -0500

    add cylinder ribs

--- ship-not-even-wrong/ship-not-even-wrong.c
+++ ship-not-even-wrong/ship-not-even-wrong.c
@@ -29,6 +29,49 @@ static void cylinder_rings(float length, float r1, float r2, int nrings)
 	}
 }
 
+float maxf(float a, float b)
+{
+	return a > b ? a : b;
+}
+
+static void cylinder_ribs(float length, float r1, float r2, int nribs)
+{
+	static int rib_fn_number = 0;
+	int i;
+	char ribfn[30], ribsetfn[30];
+	float dangle = 360.0 / nribs;
+	float angle = 0.0;
+
+	sprintf(ribfn, "ribfn_%d", rib_fn_number++);
+	sprintf(ribsetfn, "ribsetfn_%d", rib_fn_number++);
+	module(ribfn);
+		diff();
+			cyl(length, r1, r2, 1);
+			xlate(maxf(r1, r2) * 1.05, 0, 0);
+				cube(maxf(r1, r2) * 2, maxf(r1, r2) * 2, length + 2, 1);
+			end();
+			xlate(maxf(r1, r2) * -1.05, 0, 0);
+				cube(maxf(r1, r2) * 2, maxf(r1, r2) * 2, length + 2, 1);
+			end();
+			xlate(0, maxf(r1, r2), 0);
+				cube(maxf(r1, r2) * 2.1, maxf(r1, r2) * 2.1, length + 2, 1);
+			end();
+		end();
+	end();
+
+	module(ribsetfn);
+		onion();
+		for (i = 0; i < nribs; i++) {
+			rotate(angle, 0, 0, 1); 
+				call_module(ribfn);
+			end();
+			angle += dangle;
+		}
+		end();
+	end();
+	call_module(ribsetfn);
+}
+
 static void cylindrical_thing(float length, float r1, float r2)
 {
 	onion();
@@ -46,8 +89,11 @@ static void cylindrical_module(char *modulename, float length, float r1, float r
 	
 int main(int argc, char *argv[])
 {
+#if 0
 	cylindrical_module("testthing", 10, 5, 3);
 	call_module("testthing");
+#endif
+	cylinder_ribs(10, 5, 3, 5);
 	return 0;
 }
 

64ba468c5dbde99a8877e9f8338811334663959c
Stephen M. Cameron <stephenmcameron@gmail.com>
2013-08-10 08:49:15 -0500

    start cylindrical thing

--- ship-not-even-wrong/ship-not-even-wrong.c
+++ ship-not-even-wrong/ship-not-even-wrong.c
@@ -2,8 +2,24 @@
 
 #include "opencscad.h"
 
+static void cylindrical_thing(float length, float r1, float r2)
+{
+	onion();
+		cylinder(length, r1, r2);
+	endonion();
+}
+
+static void cylindrical_module(char *modulename, float length, float r1, float r2)
+{
+	module(modulename);
+		cylindrical_thing(length, r1, r2);
+	end();
+}
+	
 int main(int argc, char *argv[])
 {
+	cylindrical_module("testthing", 10, 5, 3);
+	call_module("testthing");
 	return 0;
 }
 

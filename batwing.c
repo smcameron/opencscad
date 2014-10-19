def6480602cd2c0062c9e4ed04071d72a3ac8c0f
Stephen M. Cameron <stephenmcameron@gmail.com>
2014-10-19 12:56:37 -0500

    make batwing program accept options

--- batwing.c
+++ batwing.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+#include <getopt.h>
 
 #include "opencscad.h"
 
@@ -65,6 +67,101 @@ void fingers(int nfingers, float length, float deltalength, float angle, float d
 		endrotate();
 }
 
+static void usage(void)
+{
+	fprintf(stderr, "usage: batwing ...\n");
+	exit(1);
+}
+
+static int parse_int_option(char *o, int *i)
+{
+	int rc;
+
+	rc = sscanf(o, "%d", i);
+	if (rc != 1)
+		return 1;
+	return 0;
+}
+
+static int parse_float_option(char *o, float *f)
+{
+	int rc;
+
+	rc = sscanf(o, "%f", f);
+	if (rc != 1)
+		return 1;
+	return 0;
+}
+
+static void process_options(int argc, char *argv[], float *length, int *segments, float *deltaangle,
+		int *left, float *thickness, float *taper, float *curl)
+{
+	int i, c;
+	int digit_optind = 0;
+	int optind;
+	float f;
+
+	while (1) {
+		int this_option_optind = optind ? optind : 1;
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"segments",	required_argument, 0, 's' },
+			{"angle",	required_argument, 0,  'a' },
+			{"curl",	required_argument, 0,  'c' },
+			{"left",	required_argument, 0,  'l' },
+			{"size",	required_argument, 0,  'z' },
+			{"thickness",	required_argument, 0,  't' },
+			{"taper",	required_argument, 0,  'T' },
+			{0,         0,                 0,  0 }
+		};
+
+		c = getopt_long(argc, argv, "a:c:l:s:t:T:z:", long_options, &option_index);
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'a':
+			if (parse_float_option(optarg, &f))
+				usage();
+			*deltaangle = f;
+			break;
+		case 's':
+			if (parse_int_option(optarg, &i))
+				usage();
+			*segments = i;
+			break;
+		case 'l':
+			if (parse_int_option(optarg, &i))
+				usage();
+			*left = i;
+			break;
+		case 'T':
+			if (parse_float_option(optarg, &f))
+				usage();
+			*taper = f;
+			break;
+		case 'z':
+			if (parse_float_option(optarg, &f))
+				usage();
+			*length = f;
+			break;
+		case 't':
+			if (parse_float_option(optarg, &f))
+				usage();
+			*thickness = f;
+			break;
+		case 'c':
+			if (parse_float_option(optarg, &f))
+				usage();
+			*curl = f;
+			break;
+		default:
+			usage();
+			break;
+		}
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	int segments = 10;
@@ -76,6 +173,9 @@ int main(int argc, char *argv[])
 	float deltalength = length / 8;
 	float thickness = 8.0;
 	int left = 0;
+
+	process_options(argc, argv, &length, &segments, &deltaangle, &left, &thickness, &taper, &curl);
+
 	opencscad_init();
 	scadinline("$fn = 16;\n");
 	fingers(4, length, deltalength, angle, deltaangle, left, segments,
